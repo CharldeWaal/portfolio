70c0f197a92d077906535b74980c9b893d31a916
Charl de Waal <charldewaal@Charls-Mac-mini.local>
2025-03-11 11:16:02 +0200

    add additional page

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,18 +1,22 @@
 import { Logo } from "@/components/atoms/Logo/Logo";
 import { FaChevronDown } from "react-icons/fa6";
 import { Animation } from '@/features/animations';
+import { Typography } from "@/components/molecules/Typography/Typography";
+import { TypographyVariant } from "@/components/molecules/Typography/Typography.types";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen min-w-screen">
-      <main className="bg-amber-900 min-h-screen min-w-screen grid grid-rows-[20px_1fr_20px] items-center justify-items-center">
+      <main className="min-h-screen min-w-screen grid grid-rows-[20px_1fr_20px] items-center justify-items-center">
         <div className="flex flex-col gap-8 row-start-2 items-center justify-items-center">
           <Logo title="Charl de Waal" subtitle="Film Maker & Developer" />
           <a href="#films" className="cursor-pointer text-2xl animate-bounce" ><Animation animation={'Fade'} delay={4000}><FaChevronDown /></Animation></a>
         </div>
       </main>
-      <main id='films' className="min-h-screen min-w-screen">
-
+      <main id='films' className="min-h-screen min-w-screen grid grid-rows-[20px_1fr_20px]" >
+        <div className="flex flex-col gap-8 row-start-2 justify-items-center items-center">
+          <Typography text="Films" variant={TypographyVariant.Title} testId="films-title" />
+        </div>
       </main>
     </div>
   );
